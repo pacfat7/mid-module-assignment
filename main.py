178e3353d05eb98590f49149aafef782f197b671
pacfat7 <161882821+pacfat7@users.noreply.github.com>
2024-03-02 12:46:32 +0000

    updated code

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 import sys
-def floyd_recursive(distance, intermediate=-1, start_node=0, end_node=0, MAX_LENGTH=None):
-    """
-    A recursive implementation of Floyd's algorithm.
-    """
-    # Initialize MAX_LENGTH on the first call
-    if MAX_LENGTH is None:
-        MAX_LENGTH = len(distance)
 
-    # Base case for recursion
-    if intermediate == MAX_LENGTH:
-        return
+graph = [[0, 7, sys.maxsize, 8],
+         [sys.maxsize, 0, 5, sys.maxsize],
+         [sys.maxsize, sys.maxsize, 0, 2],
+         [sys.maxsize, sys.maxsize, sys.maxsize, 0]]
+n = len(graph)  
 
-    # Recursive case for updating distances
-    if intermediate == -1:
-        # After considering all intermediates, move to the next pair of nodes
-        if end_node == MAX_LENGTH - 1:
-            if start_node == MAX_LENGTH - 2:
-                # All node pairs have been considered, start the process for intermediates
-                floyd_recursive(distance, 0, 0, 0, MAX_LENGTH)
-            else:
-                # Move to the next start node, reset end node
-                floyd_recursive(distance, -1, start_node + 1, 0, MAX_LENGTH)
-        else:
-            if start_node != end_node:
-                # Update the distance for the current pair (start_node, end_node)
-                distance[start_node][end_node] = min(distance[start_node][end_node], 
-                                                      distance[start_node][intermediate] + distance[intermediate][end_node])
-            # Move to the next end node
-            floyd_recursive(distance, -1, start_node, end_node + 1, MAX_LENGTH)
-    else:
-        # Update distances considering the current intermediate
-        floyd_recursive(distance, -1, start_node, end_node, MAX_LENGTH)
-        # Move to the next intermediate node
-        floyd_recursive(distance, intermediate + 1, 0, 0, MAX_LENGTH)
+def shortest_path(i, j, k):
+    if k < 0:
+        return graph[i][j]
+    if i == j:
+        return 0
+    without_k = shortest_path(i, j, k-1)
+    with_k = shortest_path(i, k, k-1) + shortest_path(k, j, k-1)
+    return min(without_k, with_k)
 
-# Example usage
-if __name__ == "__main__":
-    # Example distance matrix
-    graph = [[0, sys.maxsize, -2, sys.maxsize],
-             [4, 0, 3, sys.maxsize],
-             [sys.maxsize, sys.maxsize, 0, 2],
-             [sys.maxsize, -1, sys.maxsize, 0]]
-    floyd_recursive(graph)
-    print(graph)
+shortest_paths = [[None for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        shortest_paths[i][j] = shortest_path(i, j, n-1)
+
+for row in shortest_paths:
+    print(row)
